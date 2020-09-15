87bfa7ea2b782cfdcd34d8d89d3d2c9835bf6225
lukun <lukun@cmcm.com>
2020-09-15 14:23:02 +0800

    fix pkg

--- pkg/etcd/etcd.go
+++ pkg/etcd/etcd.go
@@ -2,8 +2,8 @@ package etcd
 
 import (
 	"errors"
+	"gin-learn-todo/pkg/log"
 	"go.etcd.io/etcd/clientv3"
-	"log"
 	"time"
 )
 
@@ -13,18 +13,18 @@ var (
 	LeaseKeepAliveFail = errors.New("lease keepAlive fail")
 )
 
-func Init() error{
+func Init() error {
 	var err error
 	cli, err = clientv3.New(clientv3.Config{
 		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
 		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
-		log.Fatalf("init etcd err %s", err)
+		log.Sugar().Fatalf("init etcd err %s", err)
 		return err
 	}
 	// 建立客户端成功
-	log.Printf("init etcd success")
+	log.Sugar().Info("init etcd success")
 	return nil
 }
 func mustInit() error {
